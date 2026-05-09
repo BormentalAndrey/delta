ccd597a7bb3cce00cb0ceaa17b6027e6bfb34bd5
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-05-09 18:21:25 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,22 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://www.jitpack.io") }
     }
 }
 
-rootProject.name = "AppLauncher"
+rootProject.name = "MultiAppLauncher"
 include(":app")
+include(":deltachat")
+
+// Указываем, что модуль deltachat находится в папке delta
+project(":deltachat").projectDir = file("delta")
