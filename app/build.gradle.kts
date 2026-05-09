733e5e5e7f76ba62c81642ddf82b8b457d2aa014
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-05-09 19:21:20 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,14 @@ android {
         missingDimensionStrategy("none", "foss")
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
