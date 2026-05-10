41dddd703ad22f1efb1ca6bbcbc82246f2129bcf
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-05-10 16:33:52 +0300

    Update TyrApplicationWrapper.kt

--- app/src/main/java/com/launcher/multiapp/TyrApplicationWrapper.kt
+++ app/src/main/java/com/launcher/multiapp/TyrApplicationWrapper.kt
@@ -1,23 +1,13 @@
 package com.launcher.multiapp
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
-import android.os.Handler
-import android.os.Looper
+import com.jbselfcompany.tyr.TyrApplication
 import com.jbselfcompany.tyr.data.ConfigRepository
-import com.jbselfcompany.tyr.receiver.NetworkChangeReceiver
 import com.jbselfcompany.tyr.utils.LocaleHelper
-import com.jbselfcompany.tyr.utils.TyrLogger
 
 class TyrApplicationWrapper : com.b44t.messenger.ApplicationContext() {
 
     companion object {
-        const val CHANNEL_ID_SERVICE = "yggmail_service"
-        const val CHANNEL_ID_MAIL = "mail_notifications"
-        const val CHANNEL_ID_CHAT = "chat_notifications"
-
         lateinit var instance: TyrApplicationWrapper
             private set
     }
@@ -27,34 +17,22 @@ class TyrApplicationWrapper : com.b44t.messenger.ApplicationContext() {
 
     var yggmailServiceBinder: com.jbselfcompany.tyr.service.YggmailService.LocalBinder? = null
 
-    private var networkCallback: NetworkChangeReceiver? = null
-    private val networkCallbackHandler = Handler(Looper.getMainLooper())
-    private var networkCallbackRunnable: Runnable? = null
+    // Экземпляр TyrApplication для вызова его методов
+    private lateinit var tyrApp: TyrApplication
 
     override fun onCreate() {
-        super.onCreate()
         instance = this
-        configRepository = ConfigRepository(this)
-        TyrLogger.setEnabled(configRepository.isLogCollectionEnabled())
-        LocaleHelper.applyTheme(this)
-        createNotificationChannels()
-        if (configRepository.isServiceEnabled() && configRepository.isAutoStartEnabled()) {
-            scheduleNetworkCallbackRegistration()
-        }
-    }
-
-    private fun scheduleNetworkCallbackRegistration() {
-        networkCallbackRunnable = Runnable {
-            networkCallback = NetworkChangeReceiver(this)
-            networkCallback?.register()
-        }
-        networkCallbackHandler.postDelayed(networkCallbackRunnable!!, 15000)
-    }
-
-    fun cancelNetworkCallbackRegistration() {
-        networkCallbackRunnable?.let {
-            networkCallbackHandler.removeCallbacks(it)
-        }
+        
+        // Сначала инициализируем DeltaChat
+        super.onCreate()
+        
+        // Создаём и инициализируем Tyr (он сам создаст каналы, зарегистрирует колбэки и т.д.)
+        tyrApp = TyrApplication()
+        tyrApp.attachBaseContext(this)
+        tyrApp.onCreate()
+        
+        // Копируем свойства
+        configRepository = tyrApp.configRepository
     }
 
     override fun attachBaseContext(base: Context) {
@@ -62,45 +40,7 @@ class TyrApplicationWrapper : com.b44t.messenger.ApplicationContext() {
     }
 
     override fun onTerminate() {
-        cancelNetworkCallbackRegistration()
-        networkCallback?.unregister()
+        tyrApp.onTerminate()
         super.onTerminate()
-    }
-
-    private fun createNotificationChannels() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationManager = getSystemService(NotificationManager::class.java)
-
-            val serviceChannel = NotificationChannel(
-                CHANNEL_ID_SERVICE,
-                "Yggmail Service",
-                NotificationManager.IMPORTANCE_MIN
-            ).apply {
-                description = "Yggmail background service"
-                setShowBadge(false)
-            }
-
-            val mailChannel = NotificationChannel(
-                CHANNEL_ID_MAIL,
-                "Mail Notifications",
-                NotificationManager.IMPORTANCE_DEFAULT
-            ).apply {
-                description = "New mail notifications"
-                setShowBadge(true)
-            }
-
-            val chatChannel = NotificationChannel(
-                CHANNEL_ID_CHAT,
-                "Chat Messages",
-                NotificationManager.IMPORTANCE_HIGH
-            ).apply {
-                description = "Chat message notifications"
-                setShowBadge(true)
-            }
-
-            notificationManager.createNotificationChannel(serviceChannel)
-            notificationManager.createNotificationChannel(mailChannel)
-            notificationManager.createNotificationChannel(chatChannel)
-        }
     }
 }
