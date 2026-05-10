cf6c87ff18d50d0ccc9bf6f18bbdd99e99da5eda
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-05-10 19:06:02 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,8 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.transition:transition:1.5.1")
     
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
