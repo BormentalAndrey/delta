e27a2e1884b811383299bebe58f9b87d48e89222
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-05-15 17:30:58 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,12 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.25" apply false
     id("com.google.devtools.ksp") version "1.9.25-1.0.20" apply false
 }
+
+// Отключить сжатие PNG для всех модулей
+subprojects {
+    afterEvaluate {
+        tasks.withType<com.android.build.gradle.tasks.MergeResources> {
+            isCrunchPngs = false
+        }
+    }
+}
