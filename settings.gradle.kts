8bde315d94cc2f9ca36a0a22af69d23ddb02c5a3
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-05-15 21:54:42 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,49 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
-        google()
+        // Приоритет Google репозитория для плагинов Android
+        google {
+            content {
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+                includeGroupAndSubgroups("androidx")
+            }
+        }
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
+    // FAIL_ON_PROJECT_REPOS гарантирует, что все репозитории объявлены здесь, а не в build.gradle модулей
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        
+        // JitPack необходим для сторонних библиотек, загружаемых напрямую из GitHub
+        maven { 
+            url = uri("https://jitpack.io") 
+            content {
+                includeGroup("com.github.amulyakhare") // Для TextDrawable
+                includeGroup("com.github.Baseflow")    // Для PhotoView
+                includeGroup("com.github.bumptech.glide")
+                includeGroup("com.github.davemorrissey")
+            }
+        }
     }
 }
 
+// Имя корневого проекта
 rootProject.name = "MultiAppLauncher"
 
-// Основные модули
+// Подключение модулей
 include(":app")
 include(":deltachat")
 include(":tyr")
 
-// Указываем пути к модулям в подпапках
+// Привязка имен модулей к физическим путям в файловой системе
+// Модуль :deltachat находится в папке /delta в корне проекта
 project(":deltachat").projectDir = file("delta")
+
+// Модуль :tyr находится во вложенной папке /tyr/app
 project(":tyr").projectDir = file("tyr/app")
